639477bb53a8d30d5d4e0132019775723f445b39
Eloi CHOUVET <echouvet@e1r7p21.42.fr>
2018-09-01 21:25:50 +0200

    first push

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express')
+	app = express()
+    http = require("http")
+    server = http.createServer(app);
+    mysql = require('mysql')
+    mailer = require("nodemailer")
+    bcrypt = require('bcrypt')
+    fs = require('fs')
+    css = { style : fs.readFileSync('./style.css','utf8') }
+    eschtml = require('htmlspecialchars')
+    ssn = require('express-session')
+    MemoryStore = require('session-memory-store')(ssn)
+    sessionMiddleware = ssn({ secret: "Eloi has a beautiful secret",
+        store: new MemoryStore(),
+        key: 'sid',
+        resave: true, 
+        saveUninitialized: true
+    });
+    app.use(sessionMiddleware);
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root42"
+})
+con.connect(function(err) { if (err) throw err
+    eval(fs.readFileSync(__dirname + "/back/database.js")+'')
+})
+
+server.listen(8080)
+
+app.get('/', function(req,res){
+    if (req.session.profile == undefined)
+        res.redirect('/register')
+    else
+        res.redirect('/index')
+})
+.get('/index', function(req, res) {
+    res.render('index.ejs')
+})
+.all('/login', function(req,res){
+    eval(fs.readFileSync(__dirname + "/back/login.js")+'')
+})
+.all('/register', function(req,res){
+    eval(fs.readFileSync(__dirname + "/back/register.js")+'')
+})
---
+++ back/database.js
@@ -0,0 +1,11 @@
+con.query('CREATE DATABASE IF NOT EXISTS `hypertube`', function (err) { if (err) throw err })
+con.query('USE `hypertube`', function (err) { if (err) throw err })
+var users = `CREATE TABLE IF NOT EXISTS users ( \
+    id INT AUTO_INCREMENT PRIMARY KEY, \
+    login VARCHAR(255), \
+    firstname VARCHAR(255), \
+    lastname VARCHAR(255), \
+    pass VARCHAR(255), \
+    email VARCHAR(255), \
+    img1 VARCHAR(255) DEFAULT 'empty.png')`
+con.query(users, function (err) { if (err) throw err })
