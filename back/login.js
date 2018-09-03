d14df93abe022a9d413b0cb7fd5f1d838ecb4563
Eloi CHOUVET <echouvet@e1r5p20.42.fr>
2018-09-03 18:02:43 +0200

    register fix

--- back/login.js
+++ back/login.js
@@ -7,21 +7,21 @@ else
 var login = eschtml(req.body.login)
 	pass = eschtml(req.body.pass)
 	con.query('SELECT * FROM `users` WHERE login = ?', [login], function (err, result) { if (err) throw err; 
-		if (result.length == 0)
-   			res.render('login.ejs', {req: req, error: 'Unknown Username'})
-   		else
-   		{
-   			bcrypt.compare(pass, result[0].pass, function(err, respass) 
-   			{ 
-   				if(!respass)
-   					res.render('login.ejs', {req: req, error: 'Wrong Password'})
-   				else
-   				{
-   					req.session.profile = result[0].firstname + ' ' + result[0].lastname
-   					req.session.lang = result[0].language
-   					res.redirect('/index')
-   				}
-   			})
-   		}
-   	})
+	if (result.length == 0)
+			res.render('login.ejs', {req: req, error: 'Unknown Username'})
+		else
+		{
+			bcrypt.compare(pass, result[0].pass, function(err, respass) 
+			{ 
+				if(!respass)
+					res.render('login.ejs', {req: req, error: 'Wrong Password'})
+				else
+				{
+					req.session.profile = result[0].firstname + ' ' + result[0].lastname
+					req.session.lang = result[0].language
+					res.redirect('/index')
+				}
+			})
+		}
+	})
 }
