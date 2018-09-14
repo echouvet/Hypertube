e77555dde03e46d8084099a1d028c5c4f5b9c199
Antoine Lefebvre <antoine@MacBook-Pro-de-Antoine.local>
2018-09-14 16:06:12 +0200

    fixbug

--- back/search.js
+++ back/search.js
@@ -1,7 +1,7 @@
 var query = eschtml(encodeURI(req.body.query));
 if (!empty(query))
 {
-	fetch('https://archive.org/services/search/v1/scrape?count=10&sorts=title%20desc&fields=identifier%2Cdownloads%2Ctitle%2Cmediatype%2Clanguage%2Cmonth%2Cweek%2Cpublicdate%2Cdescription%2Cavg_rating%2Cformat&q=title%3A(' + query + ')')
+	fetch('https://archive.org/services/search/v1/scrape?count=100&sorts=title%20desc&fields=identifier%2Cdownloads%2Ctitle%2Cmediatype%2Clanguage%2Cmonth%2Cweek%2Cpublicdate%2Cdescription%2Cavg_rating%2Cformat&q=title%3A(' + query + ')')
     	.then(res => res.json())
     	.then(function(json) {
 			var i = 0;
