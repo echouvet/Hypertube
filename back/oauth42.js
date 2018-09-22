2ec5b913857b043498db0c7792eaac8b958d1974
Eloi CHOUVET <echouvet@e3r9p16.42.fr>
2018-09-22 16:21:20 +0200

    header

--- back/oauth42.js
+++ back/oauth42.js
@@ -1,37 +1,111 @@
-var headers = {
-        'Accept' : 'application/json',
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/x-www-form-urlencoded'
-    }
-    var options = {
-        url: 'https://api.intra.42.fr/oauth/token',
-        method: 'POST',
-        headers: headers,
-        form: {'code': req.query.code, 'client_id': '8f7f61588b5208b39c354a41e3981495710c178f7de7af8c62906f588e55c623', 'client_secret' : 'a9e24a055faf55ed936574d7c85813aedf32081cf29057470ad813bf82063dce', 'grant_type' : 'client_credentials'}
-    }
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var resp = JSON.parse(body)
-            var options = {
-                url: 'https://api.intra.42.fr/',
-                method: 'GET',
-                headers: headers,
-                qs: {'access_token': resp.access_token}
-            }
-            request(options, function (error, response, body) {
-                    console.log(body)
+  // console.log(req.query)
+// console.log(req.body)
+ // request.post({
+ //            url: 'https://api.intra.42.fr/oauth/token',
+ //            json: true,
+ //            body: {
+ //                code: req.body.code,
+ //                grant_type: 'client_credentials',
+ //                client_id: 'eef7d6d152278701ce7c395d702828ebadcde1ca4856d591557a3d2f7426dba7',
+ //                client_secret: '104f776aba57b4ef41b852395c06d12c98149011efa7fb0cd30562bc93714279',
+ //                redirect_uri: 'http://localhost:8080/oauth42'
+ //            }
+ //        }, function (error, response, body){
+ //            if (error)
+ //                console.log(error);
+ //            else if (response.body.error) 
+ //                console.log(response.body.error);
+ //            // console.log(response)
+ //            token = response.body.access_token;
+ //             console.log(token)
+ //            request.get({
+ //                url: 'https://api.intra.42.fr/v2/me?access_token=' + token,
+ //                json: true
+ //            }, function (error, response, body){
+ //                    if (error)
+ //                        console.log(error)
+ //                    else if (response.body.error)
+ //                         console.log(response.body.error)
+ //                    console.log(body)
+ //                    res.render('login.ejs')
+ //                })
+ //        });
 
-                if (!error && response.statusCode == 200) {
-                    var resp = JSON.parse(body)
-                    console.log(resp)
-                    req.session.profile = resp;
-                    req.session.profile.img = resp.avatar_url;
-                    res.render('index.ejs', {profile:req.session.profile, success:"Success login with your 42 account"})
-                }
-                else
-    				res.render('index.ejs', {error:"something went wrong"})
-            })
-    	}
-    	else
-    		res.render('index.ejs', {error:"something went wrong"})
-	});
+
+const connectUDF = () => (
+    new Promise((resolve, reject) => {
+        request.post({
+            url: 'https://api.intra.42.fr/oauth/token',
+            json: true,
+            body: {
+                code: req.body.code,
+                grant_type: 'client_credentials',
+                client_id: 'eef7d6d152278701ce7c395d702828ebadcde1ca4856d591557a3d2f7426dba7',
+                client_secret: '104f776aba57b4ef41b852395c06d12c98149011efa7fb0cd30562bc93714279',
+                redirect_uri: 'http://localhost:8080/oauth42'
+            }
+        }, (error, response, body) => {
+            if (error)
+                resolve(error);
+            else if (response.body.error) 
+                resovle(response.body.error);
+            // console.log(response)
+            token = response.body.access_token;
+             console.log(token)
+            request.get({
+                url: 'https://api.intra.42.fr/v2/me?access_token=' + token,
+                json: true
+            }, function (error, response, body){
+                    if (error)
+                        console.log(error)
+                    else if (response.body.error)
+                         console.log(response.body.error)
+                    resolve(body)
+                })
+        });
+    })
+)
+
+connectUDF().then(result => {
+    console.log(result)
+    res.render('login.ejs')
+})
+
+// var headers = {
+//         'Accept' : 'application/json',
+//         'User-Agent':       'Super Agent/0.0.1',
+//         'Content-Type':     'application/x-www-form-urlencoded'
+//     }
+//     var options = {
+//         url: 'https://api.intra.42.fr/oauth/token',
+//         method: 'POST',
+//         headers: headers,
+//         form: { 'client_id': 'eef7d6d152278701ce7c395d702828ebadcde1ca4856d591557a3d2f7426dba7', 
+//         'redirect_uri': 'http://localhost:8080/oauth',
+//         'client_secret' : '104f776aba57b4ef41b852395c06d12c98149011efa7fb0cd30562bc93714279', 'grant_type' : 'client_credentials'}
+//     }
+//     request.post(options, function (error, response, body) {
+// console.log(response.statusCode)
+//         if (!error && response.statusCode == 200) {
+//             body = JSON.parse(body)
+            
+//             var options = {
+//                 url: 'https://api.intra.42.fr/v2/me',
+//                 qs: {access_token: body.access_token},
+//             }
+
+//             request(options, function (error2, response2, body2) {
+//                 if (!error && response.statusCode == 200) {
+//                     var bodyy2 = JSON.parse(body2)
+//                     console.log(body2)
+//                     // req.session.profile = body;
+//                     // req.session.profile.img = body.avatar_url;
+//                     res.render('index.ejs', {profile:req.session.profile, success:"Success login with your 42 account"})
+//                 }
+//                 else
+//     				res.render('index.ejs', {error:"something went wrong"})
+//             })
+//     	}
+//     	else
+//     		res.render('index.ejs', {error:"something went wrong"})
+// 	});
