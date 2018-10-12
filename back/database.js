c2115d67a3521b4db99f7bf2596e305663507797
Eloi CHOUVET <echouvet@e1r10p21.42.fr>
2018-10-12 16:36:41 +0200

    putain de table de vues pour la database

--- back/database.js
+++ back/database.js
@@ -1,14 +1,21 @@
 con.query('CREATE DATABASE IF NOT EXISTS `hypertube`', (err) => { if (err) throw err })
 con.query('USE `hypertube`', (err) => { if (err) throw err })
 var users = `CREATE TABLE IF NOT EXISTS users ( \
-	    id INT AUTO_INCREMENT PRIMARY KEY, \
-	    login VARCHAR(255), firstname VARCHAR(255), \
-	    lastname VARCHAR(255), pass VARCHAR(255), \
-	    email VARCHAR(255), language VARCHAR(255), \
-	    api INT, img VARCHAR(255) )`
+id INT AUTO_INCREMENT PRIMARY KEY, \
+login VARCHAR(255), firstname VARCHAR(255), \
+lastname VARCHAR(255), pass VARCHAR(255), \
+email VARCHAR(255), language VARCHAR(255), \
+api INT, img VARCHAR(255) )`;
 con.query(users, (err) => { if (err) throw (err) }) 
 
-var movies =	'CREATE TABLE IF NOT EXISTS movies ( \
-				id INT AUTO_INCREMENT PRIMARY KEY, \
-				hash TEXT, path VARCHAR(255))'
+var movies = 'CREATE TABLE IF NOT EXISTS movies ( \
+id INT AUTO_INCREMENT PRIMARY KEY, \
+hash VARCHAR(255), path VARCHAR(255), \
+api_id INT, api INT)';
 con.query(movies, (err) => {if (err) throw err});
+
+var vues = 'CREATE TABLE IF NOT EXISTS vues (\
+id INT AUTO_INCREMENT PRIMARY KEY, \
+user_id INT, \
+movie_id INT)';
+con.query(vues, (err) => {if (err) throw err});
