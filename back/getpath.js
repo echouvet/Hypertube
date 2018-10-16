026bdfc1e96edd45ac5e24fb2268b5d2155cd4c9
Lucien LACAZE <llacaze@e2r10p23.42.fr>
2018-10-16 20:26:46 +0200

    adding timeout before launch the movie

--- back/getpath.js
+++ back/getpath.js
@@ -1,16 +1,27 @@
+
+console.log('foewjfiowejofjwoefjweifjwoiejfoweifjwiejfoji' +req.body);
 if (req.body.movie !== undefined && req.body.hash !== undefined)
 {
+	console.log('okok')
 	con.query('SELECT path FROM movies WHERE hash = ?', [req.body.hash], (err, rows) => {
 		if (err) res.redirect('/error/SQL error ' + err);
 		if (rows[0] !== undefined)
 		{
-			res.setHeader('Content-Type', 'text/plain');
-			return res.end(JSON.stringify(rows[0].path))
+			var response = {
+				status  : 200,
+				success : 'Updated Successfully',
+				path	: rows[0].path
+			}
+			res.end(JSON.stringify(response));
 		}
 		else
 		{
-			res.setHeader('Content-Type', 'text/plain');
-			return res.end(JSON.stringify('tmpo'))
+			var response = {
+				status  : 200,
+				success : 'Updated Successfully',
+				path	: '/tmpo'
+			}
+			res.end(JSON.stringify(response));
 		}
 	})
 }
