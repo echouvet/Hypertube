688f18ef2e50692cd144d7fe521b197194126407
Eloi CHOUVET <echouvet@e1r7p21.42.fr>
2018-11-15 18:03:54 +0100

    else update

--- back/comment.js
+++ back/comment.js
@@ -1,6 +1,6 @@
 if (!req.session.profile)
 	res.redirect('/')
-if (!empty(req.body.comment))
+else if (!empty(req.body.comment))
 {
 	var comment = eschtml(req.body.comment);
 	var movie_id = eschtml(req.body.movie_id);
